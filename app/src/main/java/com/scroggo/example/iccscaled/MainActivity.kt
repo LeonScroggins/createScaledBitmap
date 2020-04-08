f93356149b51a0f64f49ec9e3422925acd2ca59c
Leon Scroggins III <scroggo@google.com>
2020-04-08 11:28:43 -0400

    Reproduce createScaledBitmap bug
    Bug: 153410582
    Test: This
    
    Add one image for now.

--- app/src/main/java/com/scroggo/example/iccscaled/MainActivity.kt
+++ app/src/main/java/com/scroggo/example/iccscaled/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.scroggo.example.iccscaled
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val images = listOf<String>("EPSON  Gray - Gamma 2.2.jpg")
+        for ((index, image) in images.withIndex()) {
+            val imageView = findViewById<ImageView>(R.id.image_0_normal)
+            val source = ImageDecoder.createSource(assets, image)
+            val bitmap = ImageDecoder.decodeBitmap(source)
+            imageView.setImageBitmap(bitmap)
+
+            val scaledImageView = findViewById<ImageView>(R.id.image_0_scaled)
+            val scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width / 4,
+                    bitmap.height / 4, true)
+            scaledImageView.setImageBitmap(scaledBitmap)
+        }
     }
 }
