cd4dc3671bd05973a3bc9fe7c9e0c2c8f2680336
Leon Scroggins III <scroggo@google.com>
2020-04-08 11:47:15 -0400

    Add the other images
    TODO: Fix spacing

--- app/src/main/java/com/scroggo/example/iccscaled/MainActivity.kt
+++ app/src/main/java/com/scroggo/example/iccscaled/MainActivity.kt
@@ -8,18 +8,26 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
+    private fun getId(num: Int, scaled: Boolean): Int {
+        return resources.getIdentifier("image_${num}_${if (scaled) "scaled" else "normal"}",
+                "id", packageName)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val images = listOf<String>("EPSON  Gray - Gamma 2.2.jpg")
+        val images = listOf<String>("EPSON  Gray - Gamma 2.2.jpg",
+                "Generic Gray Gamma 2.2 Profile.jpg",
+                "Happy-Test-Screen_Generic Gray Gamma 2.2 Profile.jpg",
+                "Test-Screen_GIMP built-in D65 Grayscale with sRGB TRC.jpg")
         for ((index, image) in images.withIndex()) {
-            val imageView = findViewById<ImageView>(R.id.image_0_normal)
+            val imageView = findViewById<ImageView>(getId(index, false))
             val source = ImageDecoder.createSource(assets, image)
             val bitmap = ImageDecoder.decodeBitmap(source)
             imageView.setImageBitmap(bitmap)
 
-            val scaledImageView = findViewById<ImageView>(R.id.image_0_scaled)
+            val scaledImageView = findViewById<ImageView>(getId(index, true))
             val scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width / 4,
                     bitmap.height / 4, true)
             scaledImageView.setImageBitmap(scaledBitmap)
